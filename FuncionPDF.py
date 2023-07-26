af927c50efd2a38cd7da4e43362a61109f6a21a0
lizetadriana <adriana.lizet.mata.cano@gmail.com>
2023-07-26 10:12:36 -0600

    Agregado de QR

--- FuncionPDF.py
+++ FuncionPDF.py
@@ -1,8 +1,11 @@
 from reportlab.pdfgen import canvas
+from FuncionQR import *
 ruta = "C:/Users/adria/OneDrive/Escritorio/Modularidad en Python/Prueba de funciones/"
 nombreArchivo=ruta + "reporteGlobal.pdf"
+nombreQR=ruta+"miQR.png"
 
 def generarPDF(listaNombres,listaEdades):
+    generarQR(nombreQR, "Hola desde funcion")
     c=canvas.Canvas(nombreArchivo)
     xInicial=200
     yInicial=700
@@ -14,5 +17,8 @@ def generarPDF(listaNombres,listaEdades):
         yInicial=yInicial-20
         c.drawString(xInicial,yInicial,listaEdades[i])
         c.drawString(xInicial+120,yInicial,listaNombres[i])
+
+        c.drawImage(nombreQR,200,400,100,100)
+
     c.save()
     print("Reporte generado------")
--- FuncionQR.py
+++ FuncionQR.py
@@ -1,6 +1,6 @@
 import qrcode
-ruta= "C:/Users/adria/OneDrive/Escritorio/Modularidad en Python/Prueba de funciones/"
-img = qrcode.make("Elaborado por Adriana Mata")
-f = open(ruta+"miQR.png", "wb")
-img.save(f)
-f.close()
+def generarQR(nombreQR, informacion):
+    img = qrcode.make(informacion)
+    f = open(nombreQR, "wb")
+    img.save(f)
+    f.close()
